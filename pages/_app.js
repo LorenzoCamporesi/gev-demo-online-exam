4b4f25afbef53e7a5e6f6ff2b32dc85da6aba3ff
lorenzocamporesi <info@lorenzocamporesi.it>
2022-05-08 17:39:40 +0200

    feat: added credits added titles fixed json

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import '../styles/global.css'
+import Head from "next/head";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Prova quiz GEV 2022</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
