ac43ab135397be07992cd511d8c272488a5ced63
lorenzocamporesi <info@lorenzocamporesi.it>
2022-05-08 17:39:17 +0200

    feat: added credits added titles fixed json

--- layout/layout.js
+++ layout/layout.js
@@ -8,7 +8,11 @@ export default function Layout({ children }) {
     return (
         <>
             <div className="p-5 pt-10 lg:px-36 xl:px-80 2xl:px-96">
+                <h1 className="mb-5 text-3xl text-center font-bold">Prova quiz GEV 2022</h1>
                 <main>{children}</main>
+            </div>
+            <div className="p-3 mt-20 text-center">
+                Sviluppato da <a target="_blank" href="https://lorenzocamporesi.it" className="underline hover:text-green-600">Lorenzo Camporesi</a> per l'ambiente 🌱
             </div>
         </>
     )
